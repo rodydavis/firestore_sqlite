cd09b865df8866f886db440e4229e6a36baadb07
Rody Davis <rody.davis.jr@gmail.com>
2022-11-08 10:55:41 -0800

    adding firebase emulators

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firestore_sqlite/firestore_sqlite.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  if (kDebugMode) {
+    const host = '127.0.0.1';
+    FirebaseFirestore.instance.useFirestoreEmulator(host, 8080);
+  }
   runApp(const MyApp());
 }
 
@@ -12,6 +24,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
